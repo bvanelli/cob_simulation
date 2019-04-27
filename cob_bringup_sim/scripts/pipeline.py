65172938903812a1e6ecd3d8cdaab09758cf0d19
Brunno Vanelli <brunnovanelli@gmail.com>
2019-04-27 22:12:25 +0200

    Adding parse_data file to launch parser

---
+++ cob_bringup_sim/scripts/pipeline.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import rospy
+import tf
+import numpy as np
+import geometry_msgs.msg
+from nav_msgs.msg import Odometry
+from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import Transform, TransformStamped
+import matplotlib.pyplot as plt
+
+
+class PoseComparison:
+
+    def __init__(self):
+        # register shutdown hooks
+        rospy.on_shutdown(self.shutdown)
+        # tf utils
+        self.tl = tf.TransformListener()
+        self.br = tf.TransformBroadcaster()
+        # subscribers
+        rospy.Subscriber('/base_pose_ground_truth', Odometry, self.ground_truth_callback, queue_size=1)
+        rospy.Subscriber('/tf', TFMessage, self.tf_callback, queue_size=1)
+        # store last pose
+        self.last_pose = Transform()
+        # list of poses
+        self.pose_history = list()
+        self.i = 0
+
+    def ground_truth_callback(self, odometry):
+        # Function that republishes ground truth data as a tf with extrapolation
+        self.ground_truth = TransformStamped()
+        self.ground_truth.header = odometry.header
+        self.ground_truth.child_frame_id = 'ground_truth'
+        self.ground_truth.transform.translation.x = odometry.pose.pose.position.x
+        self.ground_truth.transform.translation.y = odometry.pose.pose.position.y
+        self.ground_truth.transform.translation.z = odometry.pose.pose.position.z
+        self.ground_truth.transform.rotation = odometry.pose.pose.orientation
+        self.br.sendTransformMessage(self.ground_truth)
+
+    def tf_callback(self, tf_message):
+        for transform in tf_message.transforms:
+            if transform.header.frame_id == 'map' and transform.child_frame_id == 'odom_combined':
+                # check if pose was updated by slam algorithm
+                if not transform.transform == self.last_pose:
+                    # calculate extrapolation of ground truth in that point
+                    self.tl.waitForTransform('map', 'ground_truth', transform.header.stamp, rospy.Duration(1))
+                    # push pose into pose history (ground truth and slam)
+                    pose = dict()
+                    pose['ground_truth'] = self.tl.lookupTransform('map', 'ground_truth', transform.header.stamp)
+                    pose['slam'] = self.tl.lookupTransform('map', 'base_footprint', transform.header.stamp)
+                    self.pose_history.append(pose)
+                    rospy.loginfo('Pose #{}\n{}'.format(self.i, pose))
+                    # finally update pose and increment pose count
+                    self.last_pose = transform.transform
+                    self.i = self.i + 1
+
+    def shutdown(self):
+        vertices = np.zeros((len(self.pose_history), 4))
+
+        for index, entry in enumerate(self.pose_history):
+            vertices[index, :2] = entry['ground_truth'][0][0:2]
+            vertices[index, 2:] = entry['slam'][0][0:2]
+        print(vertices)
+        plt.axis('equal')
+        plt.scatter(vertices[:, 0], vertices[:, 1])
+        plt.scatter(vertices[:, 2], vertices[:, 3], c='red')
+        plt.show()
+
+
+if __name__ == "__main__":
+    rospy.init_node('pipeline_node')
+
+    try:
+        a = PoseComparison()
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
