Fixed future extrapolation bug, added robot angle calculation.

cob_bringup_sim/scripts/pipeline.py
@@ -16,7 +16,7 @@ class PoseComparison:
         # register shutdown hooks
         rospy.on_shutdown(self.shutdown)
         # tf utils
-        self.tl = tf.TransformListener()
+        self.tl = tf.TransformListener(rospy.Duration(5))
         self.br = tf.TransformBroadcaster()
         # subscribers
         rospy.Subscriber('/base_pose_ground_truth', Odometry, self.ground_truth_callback, queue_size=1)
@@ -45,6 +45,7 @@ class PoseComparison:
                 if not transform.transform == self.last_pose:
                     # calculate extrapolation of ground truth in that point
                     self.tl.waitForTransform('map', 'ground_truth', transform.header.stamp, rospy.Duration(1))
+                    self.tl.waitForTransform('map', 'base_footprint', transform.header.stamp, rospy.Duration(1))
                     # push pose into pose history (ground truth and slam)
                     pose = dict()
                     pose['ground_truth'] = self.tl.lookupTransform('map', 'ground_truth', transform.header.stamp)
@@ -57,11 +58,15 @@ class PoseComparison:
 
     def shutdown(self):
         vertices = np.zeros((len(self.pose_history), 4))
+        angles = np.zeros((len(self.pose_history), 2))
 
         for index, entry in enumerate(self.pose_history):
             vertices[index, :2] = entry['ground_truth'][0][0:2]
             vertices[index, 2:] = entry['slam'][0][0:2]
-        print(vertices)
+            angles[index, 0] = tf.transformations.euler_from_quaternion(entry['ground_truth'][1])[2]
+            angles[index, 1] = tf.transformations.euler_from_quaternion(entry['slam'][1])[2]
+        print('Vertices are', vertices)
+        print('Angles are', angles)
         plt.axis('equal')
         plt.scatter(vertices[:, 0], vertices[:, 1])
         plt.scatter(vertices[:, 2], vertices[:, 3], c='red')