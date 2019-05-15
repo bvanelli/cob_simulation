69cd8b61cc6b011c675abb8d566df65dda5e9618
Brunno Vanelli <brunnovanelli@gmail.com>
2019-05-15 17:14:59 -0300

    Starting automated tests, wish me luck

--- cob_bringup_sim/launch/cartographer.lua
+++ cob_bringup_sim/launch/cartographer.lua
@@ -1,17 +1,3 @@
--- Copyright 2016 The Cartographer Authors
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---      http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
-
 include "map_builder.lua"
 include "trajectory_builder.lua"
 
@@ -19,10 +5,10 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link",
-  published_frame = "base_link",
+  tracking_frame = "base_footprint",
+  published_frame = "odom_combined",
   odom_frame = "odom_combined",
-  provide_odom_frame = true,
+  provide_odom_frame = false,
   use_odometry = false,
   num_laser_scans = 1,
   num_multi_echo_laser_scans = 0,
@@ -30,7 +16,7 @@ options = {
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
-  pose_publish_period_sec = 5e-3,
+  pose_publish_period_sec = 2,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
@@ -40,5 +26,6 @@ options = {
 MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 10
 TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 
 return options
---
+++ cob_bringup_sim/results/icp_map_comparison.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import cv2
+import time
+import icp
+import argparse
+import matplotlib.pyplot as plt
+
+
+def read_image(im):
+    height, width = im.shape
+    black_pixels = np.sum(im == 0)
+
+    points = np.zeros((black_pixels, 3))
+
+    count = 0
+    for i in range(0, height):
+        for j in range(0, width):
+            if im[i, j] == 0:
+                points[count, :] = np.array([i, j, 0])
+                count = count + 1
+
+    return points
+
+
+def test_match(map_name, image_name, tolerance):
+    im_model_raw = cv2.imread(map_name, cv2.IMREAD_GRAYSCALE)
+    _, im_model = cv2.threshold(
+        im_model_raw, 254, 255, cv2.THRESH_BINARY)
+
+    im_raw = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+    _, im = cv2.threshold(im_raw, 1, 255, cv2.THRESH_BINARY)
+
+    points_model = read_image(im_model)
+    points = read_image(im)
+
+    np.random.shuffle(points_model)
+    extension = points.shape[0] - points_model.shape[0]
+    points_model_upsampled = np.append(
+        points_model, points_model[0:extension, :], axis=0)
+
+    T, distances, iterations = icp.icp(
+        points, points_model_upsampled, tolerance=tolerance)
+
+    # Make C a homogeneous representation of B
+    points_transformed = np.ones((points.shape[0], 4))
+    points_transformed[:, 0:3] = points
+
+    # Transform C
+    points_transformed = np.dot(T, points_transformed.T).T
+
+    print('Transformation Matrix:', T)
+
+    print('Point distances:', distances)
+
+    print('Pixel squared error:', np.average(distances ** 2))
+
+    print('Number of iterations:', iterations)
+
+    plt.plot(points_model[:, 0], points_model[:, 1], 'ro',
+             points_transformed[:, 0], points_transformed[:, 1], 'bo')
+    plt.axis('equal')
+    plt.show()
+
+
+if __name__ == "__main__":
+    rospy.init_node('icp_map_comparison_node')
+
+    parser = argparse.ArgumentParser(
+        description='Calculates the minimal transformation between two maps.')
+    parser.add_argument('-m', '--map', type=str,
+                        help='The input map name.')
+    parser.add_argument('-i', '--image', type=str,
+                        help='The input image name.')
+    parser.add_argument('-t', '--tolerance', type=float,
+                        help='ICP tolerance (default is 0.0001).', default=0.0001)
+    args, _ = parser.parse_known_args()
+
+    test_match(args.map, args.image, args.tolerance)
